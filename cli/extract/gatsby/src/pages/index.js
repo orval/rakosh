73f8d101ff301454627a119a4c1c18b10cfbf030
Duncan Rance <drance@veracode.com>
2023-01-10 00:08:12 +0000

    wip

--- cli/extract/gatsby/src/pages/index.js
+++ cli/extract/gatsby/src/pages/index.js
@@ -3,19 +3,21 @@ import * as styles from './index.module.css'
 
 const IndexPage = () => {
   return (
-    <main>
+    <>
       <div className={styles.container}>
         <div className={styles.topbar}>
           <h1>Top</h1>
         </div>
-        <div className={styles.sidebar}>
-          <h1>Side</h1>
-        </div>
-        <div className={styles.content}>
-          <h1>Content</h1>
+        <div className={styles.main}>
+          <div className={styles.sidebar}>
+            <h1>Side</h1>
+          </div>
+          <div className={styles.content}>
+            <h1>Content</h1>
+          </div>
         </div>
       </div>
-    </main>
+    </>
   )
 }
 
