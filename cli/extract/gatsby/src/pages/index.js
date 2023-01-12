befdbe0bdc9d182d83163ab99d23485887900596
Duncan Rance <drance@veracode.com>
2023-01-12 09:39:04 +0000

    wip

--- cli/extract/gatsby/src/pages/index.js
+++ cli/extract/gatsby/src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import * as styles from './index.module.css'
+import { Link } from 'gatsby'
 
 const IndexPage = () => {
   return (
@@ -11,6 +12,14 @@ const IndexPage = () => {
         <div className={styles.main}>
           <div className={styles.sidebar}>
             <h1>Side</h1>
+            <ul>
+              <li>
+                <Link to="/nugget/49b70b98-48be-42f3-b314-ccfa469f7987/">A Useful Nugget</Link>
+              </li>
+              <li>
+                <Link to="/seam/ed742eb0-26e4-400d-af4d-4dfd715dfe85/">Seam</Link>
+              </li>
+            </ul>
           </div>
           <div className={styles.content}>
             <h1>Content</h1>
