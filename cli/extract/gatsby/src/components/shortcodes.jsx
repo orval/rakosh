fca78c732fe53fb8708190bd14374da4018e3f7c
Duncan Rance <drance@veracode.com>
2023-03-31 12:10:40 +0100

    shortcodes refactor

--- cli/extract/gatsby/src/components/shortcodes.jsx
+++ cli/extract/gatsby/src/components/shortcodes.jsx
@@ -2,57 +2,70 @@ import * as React from 'react'
 import PropTypes from 'prop-types'
 import * as styles from './shortcodes.module.css'
 
-function getStyle (props) {
-  function getDirection (props) {
-    if ('direction' in props) {
-      if (props.direction === 'inbound') {
-        return (props.type === 'passage') ? styles.left : styles.up
-      } else if (props.direction === 'outbound') {
-        return (props.type === 'passage') ? styles.right : styles.down
-      }
-    }
-    return styles.main
+function getBorderStyles (props) {
+  if (props.direction === 'inbound') {
+    return (props.type === 'passage') ? styles.left : styles.up
+  } else if (props.direction === 'outbound') {
+    return (props.type === 'passage') ? styles.right : styles.down
   }
-  let nugStyle = styles.nugget
-  if (props.type === 'passage') nugStyle = styles.passage
-  else if (props.type === 'seam') nugStyle = styles.seam
-  return `${styles.bordered} ${getDirection(props)} ${nugStyle}`
+  return styles.main
 }
 
-const Nugget = (props) => {
+function getNuggetStyle (props) {
+  if (props.type === 'passage') return styles.passage
+  if (props.type === 'seam') return styles.seam
+  return styles.nugget
+}
+
+function withNuggetPropTypes (Component) {
+  Component.propTypes = {
+    children: PropTypes.oneOfType([
+      PropTypes.object,
+      PropTypes.arrayOf(PropTypes.object)
+    ]).isRequired,
+    source: PropTypes.string.isRequired
+  }
+  return Component
+}
+
+const Nugget = withNuggetPropTypes((props) => {
+  const bordered = styles.bordered
+  const directionStyle = getBorderStyles(props)
+  const nuggetStyle = getNuggetStyle(props)
+  const className = `${bordered} ${directionStyle} ${nuggetStyle}`
   return (
-    <div className={getStyle(props)}>
-      {props.children}
-      {/* <a href={props.source}>edit</a> */}
-    </div>
-  )
-}
-
-Nugget.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.object,
-    PropTypes.arrayOf(PropTypes.object)
-  ]).isRequired,
-  source: PropTypes.string.isRequired
-}
-
-const Seam = (props) => {
-  return (
-    <div className={getStyle(props)}>
-      {props.children}
-    </div>
-  )
-}
-Seam.propTypes = Nugget.propTypes
-
-const Passage = (props) => {
-  return (
-    <div className={getStyle(props)}>
+    <div className={className}>
       {props.children}
     </div>
   )
+})
+
+const Seam = withNuggetPropTypes((props) => {
+  const bordered = styles.bordered
+  const directionStyle = getBorderStyles(props)
+  const className = `${bordered} ${directionStyle} ${styles.seam}`
+  return (
+    <div className={className}>
+      {props.children}
+    </div>
+  )
+})
+
+const Passage = withNuggetPropTypes((props) => {
+  const bordered = styles.bordered
+  const directionStyle = getBorderStyles(props)
+  const className = `${bordered} ${directionStyle} ${styles.passage}`
+  return (
+    <div className={className}>
+      {props.children}
+    </div>
+  )
+})
+
+const NuggetArea = (props) => {
+  return <div className={styles.nuggetarea}>{props.children}</div>
 }
-Passage.propTypes = Nugget.propTypes
+NuggetArea.propTypes = Nugget.propTypes
 
 const PassagesInbound = (props) => {
   return (
@@ -89,15 +102,6 @@ const NuggetsOutbound = (props) => {
   )
 }
 NuggetsOutbound.propTypes = Nugget.propTypes
-
-const NuggetArea = (props) => {
-  return (
-    <div className={styles.nuggetarea}>
-      {props.children}
-    </div>
-  )
-}
-NuggetArea.propTypes = Nugget.propTypes
 
 export const components = {
   Nugget,
