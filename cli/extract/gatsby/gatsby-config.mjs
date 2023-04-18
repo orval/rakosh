739708869d10f20642a7b52b039c1423951f3624
Duncan Rance <drance@veracode.com>
2023-04-18 14:00:37 +0100

    gatsby site uses a google font

--- cli/extract/gatsby/gatsby-config.mjs
+++ cli/extract/gatsby/gatsby-config.mjs
@@ -55,6 +55,19 @@ const config = {
             body: node.body
           }))
       }
+    },
+    {
+      resolve: 'gatsby-omni-font-loader',
+      options: {
+        enableListener: true,
+        preconnect: ['https://fonts.googleapis.com', 'https://fonts.gstatic.com'],
+        web: [
+          {
+            name: 'Figtree',
+            file: 'https://fonts.googleapis.com/css2?family=Figtree&family=Inter&display=swap'
+          }
+        ]
+      }
     }
   ]
 }
