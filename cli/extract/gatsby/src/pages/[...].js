4e15710423af8157ad5dd1377073e773bdcec80c
Duncan Rance <drance@veracode.com>
2023-08-17 17:28:03 +0100

    slug lookup takes account of configured pathPrefix

--- cli/extract/gatsby/src/pages/[...].js
+++ cli/extract/gatsby/src/pages/[...].js
@@ -3,8 +3,17 @@ import { useLocation } from '@reach/router'
 import { navigate } from 'gatsby'
 import slugLookup from '../../content/slug_lookup.json'
 
+function stripPathPrefix (path) {
+  // pathPrefix from config does not appear in generated paths so remove it
+  const pathPrefix = '{{{path_prefix}}}'
+  if (path.startsWith(pathPrefix)) {
+    return path.substring(pathPrefix.length)
+  }
+  return path
+}
+
 const SlugNav = () => {
-  const path = useLocation().pathname.replace(/\/$/, '')
+  const path = stripPathPrefix(useLocation().pathname.replace(/\/$/, ''))
 
   if (path in slugLookup) {
     navigate(slugLookup[path])
