a548ef2d2b9c00de5097acec8c4aaefa93252286
Duncan Rance <drance@veracode.com>
2023-08-24 15:45:42 +0100

    do not strip SlugNav pathPrefix when it is just one forwardslash

--- cli/extract/gatsby/src/pages/[...].js
+++ cli/extract/gatsby/src/pages/[...].js
@@ -6,6 +6,9 @@ import slugLookup from '../../content/slug_lookup.json'
 function stripPathPrefix (path) {
   // pathPrefix from config does not appear in generated paths so remove it
   const pathPrefix = '{{{path_prefix}}}'
+
+  if (pathPrefix === '/') return path // do not strip leading slash
+
   if (path.startsWith(pathPrefix)) {
     return path.substring(pathPrefix.length)
   }
