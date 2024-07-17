8f2604b1ff5409ca4b1c24465617bdf37e43d349
Duncan Rance <drance@veracode.com>
2024-07-17 15:40:22 +0100

    track depth to determine when to stop

--- cli/gentest.js
+++ cli/gentest.js
@@ -9,7 +9,9 @@ import { LoremIpsum } from 'lorem-ipsum'
 import { FsLayout } from './lib/fs_layout.js'
 
 const GENTEST_NAME = 'Rakosh Test'
-const DEPTH = 3
+const DEPTH = 4
+const BREADTH = 10
+const NUGS = 3
 
 const lorem = new LoremIpsum()
 
@@ -48,26 +50,25 @@ export default {
 
     fsLayout.createAdit(join(argv.dir, slugify(GENTEST_NAME).toLowerCase() + '.md'), GENTEST_NAME)
 
-    let parentDir = argv.dir
-    for (let i = 1; i <= DEPTH; i++) {
-      parentDir = makePassage(parentDir, fsLayout)
-    }
+    makePassages(argv.dir, fsLayout, 0)
 
     const check = new FsLayout(argv.dir)
     log.info(`created ${check.size()} entities`)
   }
 }
 
-function makePassage (parent, fsLayout) {
-  const dir = join(parent, slugify(lorem.generateWords(2)).toLowerCase())
-  mkdirSync(dir)
-  generateNuggets(fsLayout, dir)
-  return dir
+function makePassages (parent, fsLayout, depth) {
+  depth++
+  for (let i = 0; i < BREADTH && depth < DEPTH; i++) {
+    const dir = join(parent, slugify(lorem.generateWords(2)).toLowerCase())
+    mkdirSync(dir)
+    generateNuggets(fsLayout, dir)
+    makePassages(dir, fsLayout, depth)
+  }
 }
 
 function generateNuggets (fsLayout, dir) {
-  for (let i = 1; i <= 5; i++) {
-    // mkdirSync(dir)
+  for (let i = 1; i <= NUGS; i++) {
     const title = lorem.generateWords(3)
     const nugPath = join(dir, slugify(title).toLowerCase() + '.md')
     fsLayout.add(nugPath, title)
