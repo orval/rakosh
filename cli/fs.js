46f18fc3497c77e8acd0ab4981a7d622a1610779
Duncan Rance <drance@veracode.com>
2025-05-14 16:47:38 +0100

    lint

--- cli/fs.js
+++ cli/fs.js
@@ -14,24 +14,24 @@ export default {
 
   builder: (yargs) => {
     return yargs
-    .command(
-      'lint <path>',
-      'Lint files in a rakosh filesystem layout',
-      yargs => yargs
-        .positional('path', {
-          describe: 'path to a directory containing an "adit" nugget',
-          string: true,
-          normalize: true,
-          coerce: p => {
-            p = resolve(p)
-            try {
-              if (!statSync(p).isDirectory()) throw new Error()
-            } catch {
-              throw new Error(`${p} is not a directory`)
+      .command(
+        'lint <path>',
+        'Lint files in a rakosh filesystem layout',
+        yargs => yargs
+          .positional('path', {
+            describe: 'path to a directory containing an "adit" nugget',
+            string: true,
+            normalize: true,
+            coerce: p => {
+              p = resolve(p)
+              try {
+                if (!statSync(p).isDirectory()) throw new Error()
+              } catch {
+                throw new Error(`${p} is not a directory`)
+              }
+              return p
             }
-            return p
-          }
-        }),
+          }),
         argv => {
           if (argv.verbose) log.setLevel('INFO')
           let fsLayout
@@ -46,48 +46,48 @@ export default {
           log.info(`Found ${fsLayout.size()} nuggets`)
           return true
         }
-    )
-    .command(
-      'add <path> <title>',
-      'Add a new rakosh nugget or passage',
-      yargs => yargs
-        .positional('path', {
-          describe: 'Path to the nugget or passage',
-          string: true,
-          normalize: true,
-          coerce: p => {
-            p = resolve(p)
-            const dirPath = dirname(p)
-            try {
-              if (!statSync(dirPath).isDirectory()) throw new Error()
-            } catch {
-              throw new Error(`${dirPath} is not a directory`)
+      )
+      .command(
+        'add <path> <title>',
+        'Add a new rakosh nugget or passage',
+        yargs => yargs
+          .positional('path', {
+            describe: 'Path to the nugget or passage',
+            string: true,
+            normalize: true,
+            coerce: p => {
+              p = resolve(p)
+              const dirPath = dirname(p)
+              try {
+                if (!statSync(dirPath).isDirectory()) throw new Error()
+              } catch {
+                throw new Error(`${dirPath} is not a directory`)
+              }
+              const ext = extname(p)
+              if (ext !== '' && ext !== '.md') {
+                throw new Error(`Unrecognised file type for ${p}`)
+              }
+              return p
             }
-            const ext = extname(p)
-            if (ext !== '' && ext !== '.md') {
-              throw new Error(`Unrecognised file type for ${p}`)
-            }
-            return p
+          })
+          .positional('title', {
+            describe: 'Title of the new nugget',
+            type: 'string'
+          }),
+        argv => {
+          if (argv.verbose) log.setLevel('INFO')
+          let fsLayout
+
+          try {
+            fsLayout = new FsLayout(dirname(argv.path))
+          } catch (err) {
+            log.error(`Failed to obtain FsLayout ${err}`)
+            return false
           }
-        })
-        .positional('title', {
-          describe: 'Title of the new nugget',
-          type: 'string',
-        }),
-      argv => {
-        if (argv.verbose) log.setLevel('INFO')
-        let fsLayout
 
-        try {
-          fsLayout = new FsLayout(dirname(argv.path))
-        } catch (err) {
-          log.error(`Failed to obtain FsLayout ${err}`)
-          return false
+          fsLayout.add(argv.path, argv.title)
+          return true
         }
-
-        fsLayout.add(argv.path, argv.title)
-        return true
-      }
-    )
+      )
   }
 }
